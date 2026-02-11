13c3131334a9c73004c22b764c207d800fa4c98e
mick <gamingspotinc@gmail.com>
2026-02-11 13:05:38 -0500

    image7

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-
   const token_hash = searchParams.get("token");
   const type = searchParams.get("type");
 
--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,11 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      detectSessionInUrl: true, // détecte automatiquement la session depuis le magic link
-      flowType: "implicit",     // flow client-side
-    },
-  }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
