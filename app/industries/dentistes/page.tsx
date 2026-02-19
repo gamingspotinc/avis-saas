cbb595fb76243af7b95d26d4e3fba6e7c0b38215
mick <gamingspotinc@gmail.com>
2026-02-18 21:36:56 -0500

    structure2

---
+++ app/industries/dentistes/page.tsx
@@ -0,0 +1,11 @@
+export default function DentistesPage() {
+  return (
+    <main style={{ padding: "100px 20px" }}>
+      <h1>Solutions pour cliniques dentaires</h1>
+      <p>
+        AvisPME aide les cliniques dentaires à protéger leur réputation
+        et augmenter leur visibilité locale.
+      </p>
+    </main>
+  );
+}
